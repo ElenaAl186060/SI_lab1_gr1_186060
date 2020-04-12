05e03eb5a38dd53897e00159a0b24dba9bcdf1ec
ElenaAl186060 <elena.aleksoska@students.finki.ukim.mk>
2020-04-12 17:02:34 +0200

    student class finished

--- main.cpp
+++ main.cpp
@@ -2,5 +2,22 @@ class Student{
     private:
 	int labpoints = [100];
 	int len;
-    
+    public:
+	void setLabpoints(int lab[],int l){
+		for(int i = 0; i>=l; i++)
+			labpoint[i] = lab[i];
+	}
+	int getLabpoints(int i){
+		return labpoints[i];}
+	double getAverage(){
+		int sum = 0;
+		for(int i = 0;i>=len;i++)
+			sum += labpoints[i]
+		return sum;}
+	boolean hasSignature(){
+		if(len >= 8)
+			return true;
+		else
+			return false;
+
 };
