4dd4ae382d214b87d4b8600aad46dfc7acb6de8d
ElenaAl186060 <elena.aleksoska@students.finki.ukim.mk>
2020-04-12 20:59:14 +0200

    on new branch

---
+++ Student.java
@@ -0,0 +1,44 @@
+class Student {
+	String index;
+	String firstName;
+	String lastName;
+	List<Integer> labpoints =new ArrayList<Integer>();
+	
+	public Student(){
+		System.out.println("New Student object!");}
+	
+	public void setindex(String index) { this.index = index; }
+	public String getindex() { return this.index; }
+
+	public void setfirstName(String firstName) { this.firstName = firstName; }
+	public String getfirstName() { return this.firstName; }
+
+	public void setlastName(String lastName) { this.lastName = lastName; }
+	public String getlastName() { return this.lastName; }
+
+	public void setlabpoints(List<Integer> labpoints) { this.labpoints = labpoints; }
+	public String getlabpoints() { return this.labpoints; }
+
+	public double getAverage() {
+		int sum = 0;
+		for(int i=1;i<=10;i++){  
+        	sum += labpoints[i];
+		return sum;  
+    		}  
+	}
+
+	public boolean hasSignature() {
+		if(labpoints.size() >= 8)
+			return true;
+	}
+};
+
+
+class Course extends Student{
+	private List<Student> lista = new ArrayList<Student>();
+	public Course(){}
+	public averagepoints(int i){
+		return lista[i].getaverage();}
+
+
+};
