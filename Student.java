a70e22a8d666963ec3ea173fd59bd527ae5149ce
ElenaAl186060 <elena.aleksoska@students.finki.ukim.mk>
2020-04-12 20:38:49 +0200

    lab points variable added

--- Student.java
+++ Student.java
@@ -2,15 +2,33 @@ class Student {
 	String index;
 	String firstName;
 	String lastName;
-	ArrayList<Integer> list=new ArrayList<Integer>();
+	List<Integer> labpoints =new ArrayList<Integer>();
 	
-	//TODO seters & getters
+	public Student(){
+		System.out.println("New Student object!");}
+	
+	public void setindex(String index) { this.index = index; }
+	public String getindex() { return this.index; }
+
+	public void setfirstName(String firstName) { this.firstName = firstName; }
+	public String getfirstName() { return this.firstName; }
+
+	public void setlastName(String lastName) { this.lastName = lastName; }
+	public String getlastName() { return this.lastName; }
+
+	public void setlabpoints(List<Integer> labpoints) { this.labpoints = labpoints; }
+	public String getlabpoints() { return this.labpoints; }
 
 	public double getAverage() {
-		//TODO
+		int sum = 0;
+		for(int i=1;i<=10;i++){  
+        	sum += labpoints[i];
+		return sum;  
+    		}  
 	}
 
 	public boolean hasSignature() {
-		//TODO
+		if(labpoints.size() >= 8)
+			return true;
 	}
 }
